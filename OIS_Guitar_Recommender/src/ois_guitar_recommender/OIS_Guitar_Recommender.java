7fa6f93e72b912c1e4f81891acfb05717b68424f
roxana.radulescu07@gmail.com <roxana.radulescu07@gmail.com@c3848352-c6f4-33a0-2ddc-c55307951247>
2014-05-21 17:03:10 +0000

    Sync

--- OIS_Guitar_Recommender/src/ois_guitar_recommender/OIS_Guitar_Recommender.java
+++ OIS_Guitar_Recommender/src/ois_guitar_recommender/OIS_Guitar_Recommender.java
@@ -13,9 +13,13 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.InfModel;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -31,53 +35,47 @@ public class OIS_Guitar_Recommender {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        String schema = "http://wilma.vub.ac.be/~yajadoul/guitars/ns";
-        String indiv = "guitars_ns.rdf";
+        //JENA SANDBOX
+
+       
         //String ns_vcard = "http://www.w3.org/2006/vcard/ns#";
-        // String ns_guitar = "http://www.ois.org/guitar.owl#";
+        String ns_guitar = "http://www.ois.org/guitar-group#";
 
-        OntModel base = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-        OntModel individuals = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
-
-        base.read(schema, "RDF/XML");
-        base.read(indiv, "RDF/XML");
 
 // create the reasoning model using the base
-        // OntModel inf = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF, base);
-        Model inf = ModelFactory.createUnion(base, individuals);
+        // 
+     
+        //   OntModel model_res = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM_RULE_INF, model);
+//        String rule = " [SiblingRule:"
+//                + "(?a ?r ?b),"
+//                + "isLiteral(?b),"
+//                + "(?a ?s ?c),"
+//                + "isLiteral(?c),"
+//                + "notEqual(?r,?s),"
+//                + "strConcat(?r, ?t),"
+//                + "regex(?t, 'http://www\\.ois\\.org/guitar-group#.*')"
+//                + "strConcat(?s, ?u),"
+//                + "regex(?u, 'http://www\\.ois\\.org/guitar-group#.*')"
+//                + "->"
+//                + "(?r http://www.ois.org/guitar-group#sibling ?s),"
+//                + "(?s http://www.ois.org/guitar-group#sibling ?r)"
+//                + "]";
+      
 
-//        Graph graph = inf.getGraph();
-//
-//        System.out.println(graph.size());
-//
         String sparql = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
                 + "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n"
                 + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
                 + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
-                + "PREFIX ns: <http://www.ois.org/guitar-group/ns#>\n"
-                + "SELECT ?subject ?object\n"
-                + "	WHERE { ?subject ns:has_offer_part ?object }";
-        //HashSet<String> set = new HashSet<>();
-        Query query = QueryFactory.create(sparql);
-        ResultSet results = QueryExecutionFactory.create(query, inf).execSelect();
-        System.out.println("Size: "+results.getRowNumber());
-        while (results.hasNext()) {
-            QuerySolution result = results.next();
+                + "PREFIX ns: <http://www.ois.org/guitar-group#>\n"
+                + "SELECT ?subject ?object \n"
+                + "	WHERE { ?subject ns:sibling ?object}";
 
-
-            //set.add(result.getResource("?object").getNameSpace());
-
-            Resource individual = result.getResource("?object");
-            System.out.println(individual.toString());
-        }
-//
-//        for (String res : set) {
-//            System.out.println(res);
-//        }
-
-//        Iterator it = GraphUtil.findAll(inf.getGraph());
-//        while (it.hasNext()) {
-//            System.out.println("triple from OntModel " + it.next().toString());
+      
+//        while (results.hasNext()) {
+//            QuerySolution result = results.next();
+//            Resource r1 = result.getResource("?subject");
+//            Resource r2 = result.getResource("?object");
+//            System.out.println(r1.toString() + "  " + r2.toString());
 //        }
     }
 }
--- OIS_Guitar_Recommender/src/ois_guitar_recommender/Queries.java
+++ OIS_Guitar_Recommender/src/ois_guitar_recommender/Queries.java
@@ -1,55 +1,104 @@
 package ois_guitar_recommender;
 
+import com.hp.hpl.jena.ontology.OntModel;
+import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QuerySolution;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.reasoner.Reasoner;
+import com.hp.hpl.jena.reasoner.rulesys.GenericRuleReasoner;
+import com.hp.hpl.jena.reasoner.rulesys.Rule;
+
 public class Queries {
-    public static String[] query(String query) {
+
+    private InfModel model;
+    private final static String prefixes = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+            + "PREFIX owl: <http://www.w3.org/2002/07/owl#>\n"
+            + "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+            + "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+            + "PREFIX ois: <http://www.ois.org/guitar-group#>\n";
+
+    public Queries() {
+        String schema = "http://wilma.vub.ac.be/~yajadoul/guitars/ontology.rdf";
+        String indiv = "http://wilma.vub.ac.be/~yajadoul/guitars/instances.rdf";
+
+        OntModel base = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+        OntModel individuals = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
+
+        base.read(schema, "RDF/XML");
+        base.read(indiv, "RDF/XML");
+        Model union = ModelFactory.createUnion(base, individuals);
+        String rules = "http://wilma.vub.ac.be/~yajadoul/guitars/rules";
+
+        Reasoner reasoner = new GenericRuleReasoner(Rule.rulesFromURL(rules));
+        reasoner.setDerivationLogging(true);
+        model = ModelFactory.createInfModel(reasoner, union);
+    }
+
+    private ResultSet query(String query) {
         ////JENA MAGIC////
-        return new String[0];
+        Query q = QueryFactory.create(prefixes + query);
+        ResultSet results = QueryExecutionFactory.create(q, model).execSelect();
+
+        return results;
     }
-    
-    public static String queryColor(String desc) {
-        String query = "SELECT ?color WHERE {" +
-                desc + " <http://www.ois.org/guitar.owl#has_global_look> ?look." +
-                "?look <http://www.ois.org/guitar.owl#has_color> ?color.}";
-        
-        ////START JENA MAGIC////
-        String color;
-        if (desc.equals("a")) {
-            color = "white";
-        } else if (desc.equals("b")) {
-            color = "white";
-        } else if (desc.equals("c")) {
-            color = "cherry";
-        } else if (desc.equals("d")) {
-            color = "brown";
-        } else if (desc.equals("e")) {
-            color = "white";
-        } else if (desc.equals("f")) {
-            color = "cherry";
+
+    public String queryColor(String desc) {
+        String queryString = "SELECT ?color WHERE {"
+                + desc + " ois:has_global_look ?look."
+                + "?look ois:has_color ?color.}";
+
+        ResultSet results = query(queryString);
+        if (results.hasNext()) {
+            QuerySolution result = results.next();
+            Resource color = result.getResource("?color");
+            return color.toString();
         } else {
-            color = "unknown";
+            return null;
         }
+        ////START JENA MAGIC////
+//        String color;
+//        if (desc.equals("a")) {
+//            color = "white";
+//        } else if (desc.equals("b")) {
+//            color = "white";
+//        } else if (desc.equals("c")) {
+//            color = "cherry";
+//        } else if (desc.equals("d")) {
+//            color = "brown";
+//        } else if (desc.equals("e")) {
+//            color = "white";
+//        } else if (desc.equals("f")) {
+//            color = "cherry";
+//        } else {
+//            color = "unknown";
+//        }
         ////END JENA MAGIC////
-        
-        return color;
     }
-    
+
     public static String queryBrand(String desc) {
         String query = "SELECT ?brand WHERE {" + desc + " <http://www.ois.org/guitar.owl#has_brand> ?brand.}";
-        
+
         ////START JENA MAGIC////
         String brand = "Fender";
         ////END JENA MAGIC////
-        
+
         return brand;
     }
-    
+
     public static String[] queryGuitarDescriptions() {
         String query = "SELECT ?name WHERE {?guitar ns:has_product_name ?name.}";
-        
+
         ////START JENA MAGIC////
-        String[] desc = new String[] {"a", "b", "c", "d", "e", "f", "g"};
+        String[] desc = new String[]{"a", "b", "c", "d", "e", "f", "g"};
         ////END JENA MAGIC////
-        
+
         return desc;
     }
 }
