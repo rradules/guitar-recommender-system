607bbdacccfb6764261ce002c726cd750701cd96
timo.verstraeten@gmail.com <timo.verstraeten@gmail.com@c3848352-c6f4-33a0-2ddc-c55307951247>
2014-05-21 15:43:16 +0000

    Refactored queries and implemented relevant siblings without Jena.

--- trunk/OIS_Guitar_Recommender/src/ois_guitar_recommender/Features.java
+++ trunk/OIS_Guitar_Recommender/src/ois_guitar_recommender/Features.java
@@ -8,146 +8,113 @@ import java.util.HashMap;
 import org.apache.commons.collections4.bag.HashBag;
 
 public class Features {
-	public Features(HashMap<String, Integer> guitarFrequencies) {
-		this.m_nr_of_features = 2;
-		createFeatureMatrix();
-		createFeatureFrequencies(guitarFrequencies);
-	}
-	
-	public double probability(String hypothesis) {
-		ArrayList<String> features = m_feature_matrix.get(hypothesis);
-		double result = 1.0;
-		for (int i = 0; i < m_nr_of_features; ++i) {
-			int nrOfEqualValues = m_feature_frequencies.get(i).getCount(features.get(i));
-			int nrOfValues = m_feature_frequencies.get(i).size();
-			int nrOfDistinctValues = m_feature_frequencies.get(i).uniqueSet().size();
-		
-			// probability = (# values conforming to hypothesis) / (# values)
-			double probability = laplaceSmoothing(nrOfEqualValues, nrOfValues, nrOfDistinctValues, 0.05);
+    public Features(HashMap<String, Integer> guitarFrequencies) {
+        this.m_nr_of_features = 2;
+        this.m_guitar_frequencies = guitarFrequencies;
+        createFeatureMatrix();
+        createFeatureFrequencies();
+    }
 
-			// MAP with Laplace Smoothing.
-			result *= probability;
-		}
-		return result;
-	}
-        
-        public void addRelevantFeatures() {
-            double[] relevance = getRelevancePerFeature();
-            
-            //Only for color.
-            if (relevance[0] >= 0.7) {
+    public double probability(String hypothesis) {
+        ArrayList<String> features = m_feature_matrix.get(hypothesis);
+        double result = 1.0;
+        for (int i = 0; i < m_nr_of_features; ++i) {
+            int nrOfEqualValues = m_feature_frequencies.get(i).getCount(features.get(i));
+            int nrOfValues = m_feature_frequencies.get(i).size();
+            int nrOfDistinctValues = m_feature_frequencies.get(i).uniqueSet().size();
+
+            //probability = (# values conforming to hypothesis) / (# values)
+            double probability = laplaceSmoothing(nrOfEqualValues, nrOfValues, nrOfDistinctValues, 0.05);
+
+            //MAP with Laplace Smoothing.
+            result *= probability;
+        }
+        return result;
+    }
+
+    public void addRelevantFeatures() {
+        double[] relevance = getRelevancePerFeature();
+
+        //Only for color.
+        if (relevance[0] >= 0.7) {
+            //This should be more elaborate, since we know that 'color' only has one sibling,
+            //and that 'material' is not optional when color is given.
+            HashBag<String> frequencies = new HashBag<>();
+            ++m_nr_of_features;
+            for (String desc : m_feature_matrix.keySet()) {                
+                String query = "SELECT ?sibling WHERE {" +
+                        desc + " ns:has_global_look ?look." +
+                        "?look ns:has_color ?color." +
+                        "?color ns:sibling ?sibling.}";
+                
                 ////START JENA MAGIC////
-                //
-                //can we somehow infer a sibling relation?
-                //
-                //[concept A --has--> concept B]
-                //[concept A --has--> concept C]
-                //==>
-                //[concept B --is_sibling_of--> concept C]
-                //[concept C --is_sibling_of--> concept B]
-                //
-                //Then we can infer that color and material are siblings (~= under the same concept).
-                //
+                String[] relevantSiblings = new String[0];
                 ////END JENA MAGIC////
                 
-                String[] relevantSiblings; //how to store queries for these?
-            }
-        }
-	
-	private double laplaceSmoothing(int x, int N, int d, double alpha) {
-		return (x + alpha) / (N + alpha*d);
-	}
-	
-	private void createFeatureMatrix() {
-		m_feature_matrix = new HashMap<String, ArrayList<String>>();
-		
-		///// SOME JENA MAGIC /////
-		String[] guitarDescriptions = queryGuitarDescriptions();
-		///// END JENA MAGIC /////
-	
-		//Merge queries maybe
-		for (String desc : guitarDescriptions) {
-                        ArrayList<String> featureVector = new ArrayList<String>();
-
-			featureVector.add(queryColor(desc));
-			featureVector.add(queryBrand(desc));
-
-			m_feature_matrix.put(desc, featureVector);
-		}
-	}
-	
-	private void createFeatureFrequencies(HashMap<String, Integer> guitarFrequencies) {	
-		m_feature_frequencies = new ArrayList<HashBag<String>>();
-		
-		for (int i = 0; i < m_nr_of_features; ++i) {
-			m_feature_frequencies.add(new HashBag<String>());
-		}
-
-		for (String guitar : guitarFrequencies.keySet()) {
-			ArrayList<String> featureVector = m_feature_matrix.get(guitar);
-			for (int i = 0; i < m_nr_of_features; ++i) {
-				m_feature_frequencies.get(i).add(featureVector.get(i), guitarFrequencies.get(guitar));
-			}
-		}
-	}
-	
-	private String queryColor(String desc) {
-		String query = "SELECT ?color WHERE {" +
-				desc + " <http://www.ois.org/guitar.owl#has_global_look> ?look." +
-				"?look <http://www.ois.org/guitar.owl#has_color> ?color.}";
-		
-		////START JENA MAGIC////
-		String color;
-		if (desc.equals("a")) {
-			color = "white";
-		} else if (desc.equals("b")) {
-			color = "white";
-		} else if (desc.equals("c")) {
-			color = "cherry";
-		} else if (desc.equals("d")) {
-			color = "brown";
-		} else if (desc.equals("e")) {
-			color = "white";
-		} else if (desc.equals("f")) {
-			color = "cherry";
-		} else {
-			color = "unknown";
-		}
-		////END JENA MAGIC////
-		
-		return color;
-	}
-	
-	private String queryBrand(String desc) {
-		String query = "SELECT ?brand WHERE {" + desc + " <http://www.ois.org/guitar.owl#has_brand> ?brand.}";
-		
-		////START JENA MAGIC////
-		String brand = "Fender";
-		////END JENA MAGIC////
-		
-		return brand;
-	}
-	
-	private String[] queryGuitarDescriptions() {
-		return new String[] {"a", "b", "c", "d", "e", "f", "g"};
-	}
-        
-        private double[] getRelevancePerFeature() {
-            double[] entropies = new double[m_nr_of_features];
-            for (int i = 0; i < m_nr_of_features; ++i) {
-                HashBag<String> values = m_feature_frequencies.get(i);
-                double entropy = 0.0;
-                double total = values.size();
-                for (String value : values.uniqueSet()) {
-                    double probability = (values.getCount(value) / total);
-                    entropy -= probability * Math.log(probability);
+                ArrayList<String> featureVector = m_feature_matrix.get(desc);
+                for (String sibling : relevantSiblings) {
+                    featureVector.add(sibling);
+                    frequencies.add(sibling, m_guitar_frequencies.get(desc));
                 }
-                entropies[i] = 1 - entropy; //A feature with a low entropy is relevant.
             }
-            return entropies;
+            m_feature_frequencies.add(frequencies);
         }
-
-	private int m_nr_of_features;
-	private HashMap<String, ArrayList<String>> m_feature_matrix;         //Each guitar mapped to its feature vector.
-	private ArrayList<HashBag<String>> m_feature_frequencies;            //Each feature index (in the feature vectors) has a bag of values.
+    }
+ 
+    private void createFeatureMatrix() {
+        m_feature_matrix = new HashMap<>();
+        
+        ///// SOME JENA MAGIC /////
+        String[] guitarDescriptions = Queries.queryGuitarDescriptions();
+        ///// END JENA MAGIC /////
+        
+        //Merge queries maybe
+        for (String desc : guitarDescriptions) {
+            ArrayList<String> featureVector = new ArrayList<>();
+            
+            featureVector.add(Queries.queryColor(desc));
+            featureVector.add(Queries.queryBrand(desc));
+            
+            m_feature_matrix.put(desc, featureVector);
+        }
+    }
+    
+    private void createFeatureFrequencies() {
+        m_feature_frequencies = new ArrayList<>();
+        
+        for (int i = 0; i < m_nr_of_features; ++i) {
+            m_feature_frequencies.add(new HashBag<String>());
+        }
+        
+        for (String guitar : m_guitar_frequencies.keySet()) {
+            ArrayList<String> featureVector = m_feature_matrix.get(guitar);
+            for (int i = 0; i < m_nr_of_features; ++i) {
+                m_feature_frequencies.get(i).add(featureVector.get(i), m_guitar_frequencies.get(guitar));
+            }
+        }
+    }
+    
+    private double[] getRelevancePerFeature() {
+        double[] entropies = new double[m_nr_of_features];
+        for (int i = 0; i < m_nr_of_features; ++i) {
+            HashBag<String> values = m_feature_frequencies.get(i);
+            double entropy = 0.0;
+            double total = values.size();
+            for (String value : values.uniqueSet()) {
+                double probability = (values.getCount(value) / total);
+                entropy -= probability * Math.log(probability);
+            }
+            entropies[i] = 1 - entropy; //A feature with a low entropy is relevant.
+        }
+        return entropies;
+    }
+       
+    private static double laplaceSmoothing(int x, int N, int d, double alpha) {
+        return (x + alpha) / (N + alpha*d);
+    }
+    
+    private int m_nr_of_features;
+    private final HashMap<String, Integer> m_guitar_frequencies;
+    private HashMap<String, ArrayList<String>> m_feature_matrix;         //Each guitar mapped to its feature vector.
+    private ArrayList<HashBag<String>> m_feature_frequencies;            //Each feature index (in the feature vectors) has a bag of values.
 }
--- trunk/OIS_Guitar_Recommender/src/ois_guitar_recommender/Recommender.java
+++ trunk/OIS_Guitar_Recommender/src/ois_guitar_recommender/Recommender.java
@@ -1,6 +1,6 @@
-/**
- * Recommender using an ontology-driven information system.
- */
+ /**
+  * Recommender using an ontology-driven information system.
+  */
 package ois_guitar_recommender;
 
 import java.io.FileNotFoundException;
@@ -13,50 +13,50 @@ import au.com.bytecode.opencsv.CSVReader;
 import org.apache.commons.collections4.bag.HashBag;
 
 public class Recommender {
-
-	public static void main(String[] args) {
-		try {
-			if (args.length != 2) {
-				throw new IOException("Both the frequency database file and number of recommendations should be given.");
-			} else {
-				Recommender recommender = new Recommender(args[0], Integer.parseInt(args[1]));
-				recommender.recommend();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public Recommender(String file, int nrOfRecommendations) throws IOException {
-		this.m_recommendations = nrOfRecommendations;
-		this.m_frequencies = new HashMap<>();
-
-		readFrequenciesFromFile(file);
-	}
-	
-	public String[] recommend() {
-		Features features = new Features(m_frequencies);
-		double probability = features.probability("e");
-		System.out.println(probability);
-		probability = features.probability("f");
-		System.out.println(probability);
-		
-		return new String[0];
-	}
-	
-	private void readFrequenciesFromFile(String file) throws IOException {
-            CSVReader reader = new CSVReader(new FileReader(file));
-	    String[] entry;
-	    while ((entry = reader.readNext()) != null) {
-	    	if (entry.length != 2) {
-			    reader.close();
-	    		throw new IOException("The number of fields in an entry is not correct.");
-	    	}
-                m_frequencies.put(entry[0], Integer.valueOf(entry[1]));
-	    }
-	    reader.close();
-	}
-
-	private int m_recommendations;
-	private HashMap<String, Integer> m_frequencies;    //Each guitar mapped to its frequency.
+    
+    public static void main(String[] args) {
+        try {
+            if (args.length != 2) {
+                throw new IOException("Both the frequency database file and number of recommendations should be given.");
+            } else {
+                Recommender recommender = new Recommender(args[0], Integer.parseInt(args[1]));
+                recommender.recommend();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Recommender(String file, int nrOfRecommendations) throws IOException {
+        this.m_recommendations = nrOfRecommendations;
+        this.m_frequencies = new HashMap<>();
+        
+        readFrequenciesFromFile(file);
+    }
+    
+    public String[] recommend() {
+        Features features = new Features(m_frequencies);
+        double probability = features.probability("e");
+        System.out.println(probability);
+        probability = features.probability("f");
+        System.out.println(probability);
+        
+        return new String[0];
+    }
+    
+    private void readFrequenciesFromFile(String file) throws IOException {
+        CSVReader reader = new CSVReader(new FileReader(file));
+        String[] entry;
+        while ((entry = reader.readNext()) != null) {
+            if (entry.length != 2) {
+                reader.close();
+                throw new IOException("The number of fields in an entry is not correct.");
+            }
+            m_frequencies.put(entry[0], Integer.valueOf(entry[1]));
+        }
+        reader.close();
+    }
+    
+    private int m_recommendations;
+    private HashMap<String, Integer> m_frequencies;    //Each guitar mapped to its frequency.
 }
