9976fbbdded94638afbbe512eb2b1d26c3985584
roxana.radulescu07@gmail.com <roxana.radulescu07@gmail.com@c3848352-c6f4-33a0-2ddc-c55307951247>
2014-05-22 12:55:06 +0000

    Sync

--- trunk/OIS_Guitar_Recommender/src/beans/Guitar_Description.java
+++ trunk/OIS_Guitar_Recommender/src/beans/Guitar_Description.java
@@ -4,12 +4,55 @@
  */
 package beans;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ois_guitar_recommender.Queries;
+
 /**
  *
  * @author Roxana
  */
 public class Guitar_Description {
-    
-    
-    
+
+    private String desc;
+    private String color_material;
+    private String type;
+    private String size;
+    private URL imageURL;
+
+    public Guitar_Description(String desc) {
+        this.desc = desc;
+        color_material = Queries.getInstance().queryColour(desc) + " "
+                + Queries.getInstance().queryMaterial(desc);
+        type = Queries.getInstance().queryType(desc);
+        size = Queries.getInstance().querySizeString(desc);
+        try {
+            imageURL = new URL(Queries.getInstance().queryImage(desc));
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(Guitar_Description.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getColor_material() {
+        return color_material;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public URL getImageURL() {
+        return imageURL;
+    }
 }
